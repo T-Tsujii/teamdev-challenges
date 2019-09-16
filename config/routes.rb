d96edf946b2904ea380d864cd5cc1d10345b4794
T.Tsujii <ttakehisabt@gmail.com>
2019-09-16 13:21:38 +0900

    Change email text to Japanese

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'homes#index'
   get '/homes/about', to: 'homes#about'
   get '/homes/recruitment', to: 'homes#recruitment'
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks', confirmations: 'confirmations' }
   resources :users
 end
